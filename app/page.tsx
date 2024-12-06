93ff785cc059b67f5065de8e28c7bf52e9b90c0e
kodeckotiso <kodeckotiso@gmail.com>
2024-12-07 02:10:12 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Hero } from "@/components";
+import { CustomButton } from '@/components';
+
+export default function Home() {
+  return (
+      <main className="overflow-hidden">
+        <Hero />
+      </main>)
+      }
